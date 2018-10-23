323b713fcc942a4109c409a91b3585ee4c96ad27
Brummolix <Brummolix@quantentunnel.de>
2018-10-23 14:55:27 +0200

    Add some typescript type definitions

---
+++ chrome/content/thunderbird-stdlib/msgHdrUtils.js.d.ts
@@ -0,0 +1,1 @@
+declare function msgHdrGetTags (aMsgHdr:string[]);
---
+++ thunderbird.d.ts
@@ -0,0 +1,12 @@
+declare var browser:any;
+declare var Components:any;
+
+declare var Cu:any;
+declare var Cc:any;
+declare var Ci:any;
+declare var FileUtils:any;
+declare var Application:any;
+declare var MailServices:any;
+declare var AddonManager:any;
+declare function fixIterator(collection:any[],objectType:any):any;
+
