26e0e12387ba3dc2efff4b51169bafd03d8e0be9
Brummolix <Brummolix@quantentunnel.de>
2019-10-04 23:00:28 +0200

    add some more TB types

--- src/sharedAll/thunderbird.d.ts
+++ src/sharedAll/thunderbird.d.ts
@@ -75,6 +75,8 @@ declare interface BrowserInfo
 //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime
 declare interface Runtime
 {
+	lastError: null| Error;
+
 	onMessage: RuntimeMessageListeners;
 	onConnect: RuntimeConnectListener;
 	//onSuspend: RuntimeSuspendListener; -> does not exist in TB?
@@ -85,6 +87,8 @@ declare interface Runtime
 
 	getBrowserInfo(): Promise<BrowserInfo>;
 	getBackgroundPage(): Promise<BrowserWindow>;
+
+	//openOptionsPage(): Promise<void>; -> openOptionsPage is not defined
 }
 
 //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea
@@ -269,6 +273,31 @@ type ItemType = "normal" | "checkbox" | "radio" | "separator";
 
 type ViewType = "tab" | "popup" | "sidebar";
 
+//https://thunderbird-webextensions.readthedocs.io/en/latest/menus.html#menus-onclickdata
+declare interface OnClickData
+{
+	editable: boolean; 	//A flag indicating whether the element is editable (text input, textarea, etc.).
+	menuItemId: number | string; // The ID of the menu item that was clicked.
+	modifiers: string[]; // An array of keyboard modifiers that were held while the menu item was clicked.
+	button?: number; 		// An integer value of button by which menu item was clicked.
+	checked?: boolean;	// A flag indicating the state of a checkbox or radio item after it is clicked.
+	displayedFolder?: MailFolder; // The displayed folder, if the context menu was opened in the message list.
+	frameId?: number;		// The id of the frame of the element where the context menu was clicked.
+	frameUrl?: string; 	// The URL of the frame of the element where the context menu was clicked, if it was in a frame.
+	linkText?: string;	//If the element is a link, the text of that link.
+	linkUrl?: string; 	//If the element is a link, the URL it points to.
+	mediaType?: string; //One of ‘image’, ‘video’, or ‘audio’ if the context menu was activated on one of these types of elements.
+	pageUrl?: string; 	//The URL of the page where the menu item was clicked. This property is not set if the click occurred in a context where there is no current page, such as in a launcher context menu.
+	parentMenuItemId?: number | string; //The parent ID, if any, for the item clicked.
+	selectedFolder?: MailFolder; //The selected folder, if the context menu was opened in the folder pane.
+	selectedMessages?: MessageList; //The selected messages, if the context menu was opened in the message list.
+	selectionText?: string; //The text for the context selection, if any.
+	srcUrl?: string; //Will be present for elements with a ‘src’ URL.
+	targetElementId?: number; //An identifier of the clicked element, if any. Use menus.getTargetElement in the page to find the corresponding element.
+	viewType?: ViewType; //The type of view where the menu is clicked. May be unset if the menu is not associated with a view.
+	wasChecked?: boolean; //A flag indicating the state of a checkbox or radio item before it was clicked.
+}
+
 declare interface menuCreateProperties
 {
 	checked?: boolean; //The initial state of a checkbox or radio item: true for selected and false for unselected. Only one radio item can be selected at a time in a given group of radio items.
@@ -278,7 +307,7 @@ declare interface menuCreateProperties
 	enabled?: boolean; //Whether this context menu item is enabled or disabled. Defaults to true.
 	icons?: object;
 	id?: string; //The unique ID to assign to this item. Mandatory for event pages. Cannot be the same as another ID for this extension.
-	onclick?: () => void; // A function that will be called back when the menu item is clicked. Event pages cannot use this.
+	onclick?: (info: OnClickData, tab?: Tab) => void; // A function that will be called back when the menu item is clicked. Event pages cannot use this.
 	parentId?: number | string; //The ID of a parent menu item; this makes the item a child of a previously added item.
 	targetUrlPatterns?: string[]; //Similar to documentUrlPatterns, but lets you filter based on the src attribute of img/audio/video tags and the href of anchor tags.
 	title?: string; //The text to be displayed in the item; this is required unless type is ‘separator’. When the context is ‘selection’, you can use %s within the string to show the selected text. For example, if this parameter’s value is “Translate ‘%s’ to Pig Latin” and the user selects the word “cool”, the context menu item for the selection is “Translate ‘cool’ to Pig Latin”.
