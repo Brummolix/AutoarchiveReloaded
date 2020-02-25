602baed5439aedfaf9e8bda02d1a24df9b0fb568
Brummolix <Brummolix@quantentunnel.de>
2020-02-25 23:58:11 +0100

    eslint changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,20 @@ const theMode = "none";
 const path = require("path");
 const outputPath = path.resolve(__dirname, "./built/release/");
 
-module.exports = [{
-	name: "webextension", //all "standard" webextension scripts
-	mode: theMode,
-	entry: {
-		background: "./built/compile/backgroundScript/background.js",
-		options: "./built/compile/options/options.js",
-		popup: "./built/compile/popup/popup.js",
+module.exports = [
+	{
+		name: "webextension", //all "standard" webextension scripts
+		mode: theMode,
+		entry: {
+			background: "./built/compile/backgroundScript/background.js",
+			options: "./built/compile/options/options.js",
+			popup: "./built/compile/popup/popup.js",
+		},
+		output: {
+			path: outputPath,
+		},
 	},
-	output: {
-		path: outputPath,
-	},
-}, {
+	{
 		name: "experiment", //the experiment needs a different output configuration
 		mode: theMode,
 		entry: "./built/compile/webexperiment/autoarchive-api.js",
