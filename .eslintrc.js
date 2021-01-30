a5cad825a0906568dfcd110cc5f785509e4a9d05
Brummolix <Brummolix@quantentunnel.de>
2021-01-30 01:37:42 +0100

    update prettier and grunt-contrib-compress

--- .eslintrc.js
+++ .eslintrc.js
@@ -109,7 +109,7 @@ module.exports = {
 		"no-multiple-empty-lines": "error",
 		"no-new-wrappers": "error",
 		"no-shadow": "off",
-		"@typescript-eslint/no-shadow": ["error"],
+		"@typescript-eslint/no-shadow": "error",
 		"no-throw-literal": "error",
 		"no-trailing-spaces": "error",
 		"no-undef-init": "error",
