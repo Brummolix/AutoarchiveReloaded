79a87584c2baeff8afd64f11cc5762b481f22207
Brummolix <Brummolix@quantentunnel.de>
2026-09-02 01:01:35 +0200

    format webpack.config.js and inline some variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,46 @@
-//development does not work in Thunderbird, it will raise CSP errors because of an included "eval"
-//production minifies the code which I don't like because errors are all reported on line 1 then
-const theMode = "none";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
-const outputPath = path.resolve(__dirname, "./dist/release/");
-
 const { defineReactCompilerLoaderOption, reactCompilerLoader } = require('react-compiler-webpack');
 const webpack = require('webpack');
-
-const extensions = [".tsx", ".ts", ".js"];
 
 module.exports = [
 	{
 		name: "webextension", //all "standard" webextension scripts
-		mode: theMode,
+
+		//"development" does not work in Thunderbird, it will raise CSP errors because of an included "eval"
+		//"production" minifies the code which I don't like because errors are all reported on line 1 then
+		mode: "none",
 		entry: {
 			background: "./src/backgroundScript/background.ts",
 			options: "./src/options/options.tsx",
 			popup: "./src/popup/popup.tsx",
 		},
 		output: {
-			path: outputPath,
+			path: path.resolve(__dirname, "./dist/release/"),
 		},
 		module: {
-			rules: [{
-				test: /\.[mc]?[jt]sx?$/i,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: reactCompilerLoader,
-						options: defineReactCompilerLoaderOption({
-							// React Compiler options goes here
-						})
-					},
-					// babel-loader, swc-loader, esbuild-loader, or anything you like to transpile JSX should go here.
-					// If you are using rspack, the rspack's built-in react transformation is sufficient.
-					{
-						loader: 'ts-loader'
-					},
-				]
-			}],
+			rules: [
+				{
+					test: /\.[mc]?[jt]sx?$/i,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: reactCompilerLoader,
+							options: defineReactCompilerLoaderOption({}),
+						},
+						{
+							loader: "ts-loader",
+						},
+					],
+				},
+			],
 		},
 		resolve: {
-			extensions: extensions,
+			extensions: [".tsx", ".ts", ".js"],
 		},
-		plugins: [
-			new webpack.DefinePlugin({
-				'process.env.NODE_ENV': JSON.stringify('production'),
-			})
-		],
+		plugins: [new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')})],
 		experiments: {
 			css: true,
 		},
-	}
+	},
 ];
